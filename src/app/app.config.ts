bfbf02812e4b531ed1c36e97a808b05999a3dbf4
clement <clement.lin@esi-tech.net>
2025-01-08 17:15:43 +0800

    LC11

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptor/error.interceptor';
+import { loadingInterceptor } from './core/interceptor/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations(),provideHttpClient(withInterceptors([errorInterceptor,loadingInterceptor]))]
 };
